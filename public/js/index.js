ff259c7b98e69f321beaf21f1ffdee80eccb4be8
Abraham Williams <4braham@gmail.com>
2018-06-18 10:05:40 -0500

    Switch to temporary drower on smaller screens

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
   const topAppBar = new mdc.topAppBar.MDCTopAppBar(document.querySelector('.mdc-top-app-bar'));
-  const drawer = new mdc.drawer.MDCPersistentDrawer(document.querySelector('.mdc-drawer'));
-  const topAppBarNavitaion = document.querySelector('.mdc-top-app-bar__navigation-icon');
+  const topAppBarNavigation = document.querySelector('.mdc-top-app-bar__navigation-icon');
 
-  topAppBarNavitaion.addEventListener('click', event => {
+  const drawerPersistent = window.innerWidth > 825;
+  const drawerElement = document.querySelector('.mdc-drawer');
+  const drawerCssClass = drawerPersistent ? 'mdc-drawer--persistent' : 'mdc-drawer--temporary';
+  const drawerClass = drawerPersistent ? mdc.drawer.MDCPersistentDrawer : mdc.drawer.MDCTemporaryDrawer;
+  drawerElement.classList.add(drawerCssClass);
+  const drawer = new drawerClass(drawerElement);
+  if (drawerPersistent) {
+    drawer.open = drawerPersistent;
+  }
+
+  topAppBarNavigation.addEventListener('click', event => {
     event.preventDefault();
     drawer.open = !drawer.open;
   });
