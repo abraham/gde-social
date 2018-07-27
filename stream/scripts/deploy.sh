0c5cb7e2c277de8de4605919b5641804fc85fd67
Abraham Williams <4braham@gmail.com>
2018-07-27 15:23:19 -0500

    Switch to standard node

--- stream/scripts/deploy.sh
+++ stream/scripts/deploy.sh
@@ -1,3 +1,3 @@
 gcloud compute ssh stream --command 'mkdir -p stream'
-gcloud compute scp ./*.ts ./*.json ../.runtimeconfig.json stream:~/stream/
+gcloud compute scp ./*.js ./*.json ../.runtimeconfig.json stream:~/stream/
 # gcloud compute instances reset stream
